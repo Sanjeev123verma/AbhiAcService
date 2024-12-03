e2d5cc2ed2df41bcf57128585e8146814c66fc37
Sanjeev Verma <160196351+Sanjeev123verma@users.noreply.github.com>
2024-12-03 17:16:51 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,39 @@
 "use client";
 import Footer from "@/components/Footer";
 import Navigation from "@/components/Navigation";
-import WhatsApp from "@/components/Whatsapp";
 import { AuthProvider } from "@/context/authContext";
 import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 import "../globals.css";
+import Head from "next/head";
 
+const WhatsApp = dynamic(() => import("@/components/Whatsapp"), { ssr: false });
 
 export default function RootLayout({ children }) {
   const [isAdminRoute, setIsAdminRoute] = useState(false);
 
-useEffect(() => {
-  if (typeof window !== "undefined") {
-    setIsAdminRoute(window.location.pathname.startsWith("/admin"));
-  }
-}, []);  
- 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsAdminRoute(window.location.pathname.startsWith("/admin"));
+    }
+  }, []);
 
   return (
     <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="robots" content="index, follow" />
+        <title>Abhi AC Service</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="pt-0">
         <AuthProvider>
-        {!isAdminRoute && <Navigation />}
-        {children} 
-        {!isAdminRoute && <Footer />}
-        {!isAdminRoute && <WhatsApp/>}
+          {!isAdminRoute && <Navigation />}
+          <main className="flex-grow">{children}</main>
+          {!isAdminRoute && <Footer />}
+          {!isAdminRoute && <WhatsApp />}
         </AuthProvider>
       </body>
     </html>
